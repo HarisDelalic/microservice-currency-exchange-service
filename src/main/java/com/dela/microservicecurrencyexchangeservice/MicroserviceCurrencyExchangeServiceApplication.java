89d802f455ee25890d5a2d126afd9cba11a57c0d
delalic <delalicharis@gmail.com>
2019-10-27 01:44:14 +0200

    intoduce eureka, microservice becomes eureka client

--- src/main/java/com/dela/microservicecurrencyexchangeservice/MicroserviceCurrencyExchangeServiceApplication.java
+++ src/main/java/com/dela/microservicecurrencyexchangeservice/MicroserviceCurrencyExchangeServiceApplication.java
@@ -2,8 +2,12 @@ package com.dela.microservicecurrencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//Same as @EnableDiscoveryClient when Eureka is on classpath
+@EnableEurekaClient
 public class MicroserviceCurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
